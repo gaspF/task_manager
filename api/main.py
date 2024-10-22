fbb91860e389760df8e8d9b8d023cd81ce89c168
GaspardHome <gaspard.fouche@connecting-food.com>
2024-10-22 12:47:00 +0200

    adding dockerfile and docker compose

--- api/main.py
+++ api/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from .database import engine, Base, get_db
-from . import crud, schemas
-from .models import Task
+from database import engine, Base, get_db
+import crud, schemas
+from models import Task
 
 # Créer la base de données
 Base.metadata.create_all(bind=engine)
